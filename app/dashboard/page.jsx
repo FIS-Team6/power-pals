364f8a1c6115419b420e88b78fb7c71becf6978d
KeinoC <keino.chichester@gmail.com>
2023-09-25 18:18:26 -0400

    pushing ai init

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import AITest from '../../components/DashboardComponents/AITest';
 
 export default function DashboardPage() {
 
     return (
         <main>
-            <h1 className=''>Dashboard</h1>            
+            <h1 className=''>Dashboard</h1>
+            <AITest />
         </main>
     )
 }
