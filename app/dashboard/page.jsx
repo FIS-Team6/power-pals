593bc53ead0976f124af9dda930e5d15892cfed4
KeinoC <keino.chichester@gmail.com>
2023-09-26 01:50:53 -0400

    new task form init

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import AITest from '../../components/DashboardComponents/AITest';
+import GPTComponent from '../../components/DashboardComponents/gpt';
+import NewTask from '../../components/DashboardComponents/NewTask';
+
+
 
 export default function DashboardPage() {
 
     return (
         <main>
             <h1 className=''>Dashboard</h1>
-            <AITest />
+            <NewTask />
+            <GPTComponent />
+            
+            {/* <AITest /> */}
         </main>
     )
 }
---
+++ components/DashboardComponents/NewTask.jsx
@@ -0,0 +1,92 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import DatePicker from "react-datepicker";
+
+const showModal = async () => {
+    try {
+        const modalElement = document.getElementById("new-task");
+        const bodyElement = document.body;
+
+        if (modalElement && bodyElement) {
+            modalElement.showModal();
+            bodyElement.classList.add("backdrop-blur-lg", "bg-slate-800");
+        }
+    } catch (error) {
+        console.error("Error showing modal:", error);
+    }
+};
+
+const closeModal = async () => {
+    try {
+        const modalElement = document.getElementById("new-task");
+        const bodyElement = document.body;
+
+        if (modalElement && bodyElement) {
+            modalElement.close();
+            bodyElement.classList.remove("backdrop-blur-lg", "bg-slate-800");
+        }
+    } catch (error) {
+        console.error("Error closing modal:", error);
+    }
+};
+
+export default function NewTask() {
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+
+    
+
+    return (
+        <div className="container">
+            <button className="btn" onClick={showModal}>
+                Add Task
+            </button>
+            <dialog
+                id="new-task"
+                className="modal modal-bottom lg:modal-middle min-h-[50%]"
+                onClose={closeModal}
+            >
+                <div className="modal-box flex flex-grow bg-red-500">
+                    <div className="modal-action flex flex-col relative">
+                        <form
+                            className="flex flex-col relative"
+                            method="dialog"
+                        >
+                            <input
+                                className="relative mb-2"
+                                type="text"
+                                placeholder="Task Name"
+                            />
+                            <textarea
+                                className="mb-2 flex-grow"
+                                type="text"
+                                placeholder="Task Description"
+                            />
+                            <div className="flex flex-row">
+                                <div>
+                                    <label>from</label>
+                                    <DatePicker
+                                        className="w-auto"
+                                        selected={startDate}
+                                        onChange={(date) => setStartDate(date)} //only when value has changed
+                                    />
+                                </div>
+
+                                <div>
+                                    <label>To:</label>
+                                    <DatePicker
+                                        selected={endDate}
+                                        onChange={(date) => setEndDate(date)} //only when value has changed
+                                    />
+                                </div>
+                            </div>
+                            <button className="btn " type="submit">
+                                Submit
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+        </div>
+    );
+}
